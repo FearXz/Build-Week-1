8520fff7ffd8b0594e69b9bd2996ab1bd7251434
Omar Lo Giudice <146293874+FearXz@users.noreply.github.com>
2023-11-16 22:01:53 +0100

    fix stelline

--- assets/js/feedback.js
+++ assets/js/feedback.js
@@ -1,32 +1,55 @@
 function createStars() {
     let starContainer = document.getElementById('rating');
-    for (let i = 0; i < 10; i++){
-        starContainer.innerHTML+=`<img src="assets/img/star.svg" alt="stellina rating" class="opacityStar">`;
+    for (let i = 0; i < 10; i++) {
+        starContainer.innerHTML += `<img src="assets/img/star.svg" alt="stellina rating" class="opacityStar">`;
     }
 }
 
 function feedbackStars() {
     let stars = document.querySelectorAll("#rating img");
+    let lastclicked;
     let clicked = false;
 
     stars.forEach(function (star, index) {
         star.addEventListener("click", function () {
-            if(clicked == false){clicked=true}
-            else if (clicked == true){clicked=false}
 
-            // Aggiungi la classe "opacityStar" da tutte le stelle
-            stars.forEach(function (s) {
-                s.classList.add("opacityStar");
-            });
+            if (clicked == false && lastclicked != index) {
+                lastclicked = index;
+                clicked = true;
 
-            // Rimuove la classe "opacityStar" solo alle stelle fino a quella cliccata
-            for (let i = 0; i <= index; i++) {
-                stars[i].classList.remove("opacityStar");
+
+                // Aggiungi la classe "opacityStar" da tutte le stelle
+                stars.forEach(function (s) {
+                    s.classList.add("opacityStar");
+                });
+
+                // Rimuove la classe "opacityStar" solo alle stelle fino a quella cliccata
+                for (let i = 0; i <= index; i++) {
+                    stars[i].classList.remove("opacityStar");
+                }
             }
+            // else if funziona dal secondo click in poi
+            else if (clicked == true && lastclicked != index) {
+                lastclicked = index;
+
+                stars.forEach(function (s) {
+                    s.classList.add("opacityStar");
+                });
+
+                for (let i = 0; i <= index; i++) {
+                    stars[i].classList.remove("opacityStar");
+                }
+            }
+            else if (lastclicked == index) {
+                clicked = false
+
+            }
+
+
         });
 
         star.addEventListener("mouseover", function () {
-            if (!clicked) {
+            if (clicked==false) {
                 // Se non è stato cliccato, colora la stella e le precedenti
                 for (let i = 0; i <= index; i++) {
                     stars[i].classList.remove("opacityStar");
@@ -35,7 +58,7 @@ function feedbackStars() {
         });
 
         star.addEventListener("mouseout", function () {
-            if (!clicked) {
+            if (clicked==false) {
                 // Se non è stato cliccato, reimposta tutte le stelle a trasparenti
                 stars.forEach(function (s) {
                     s.classList.add("opacityStar");
